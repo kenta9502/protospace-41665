c23a5bb77368869e8f357d7c0e457db334ee70f2
kenta9502 <tatata9502@gmail.com>
2025-04-01 21:31:09 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,67 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]  
+  before_action :set_prototype, except: [:index, :new, :create, :edit]
+
+  def index
+    @prototypes = Prototype.includes(:user)
+  end
+
+  def new
+    @prototype = Prototype.new
+    @comment = Comment.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  @prototype = Prototype.find(params[:id])
+  @comment = Comment.new
+  @comments = @prototype.comments
+  #@comments = 
+  end
+
+  def edit
+  @prototype = Prototype.find(params[:id])
+  @comment = Comment.find(params[:id])
+  end
+  
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to @prototype
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  @prototype = Prototype.find(params[:id])
+  @prototype.destroy
+  redirect_to root_path
+    
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
+end
